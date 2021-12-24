843725615d13f31a3d60aae5f5259cc2bf7c8b44
Sheldon Bish <sbish33@gmail.com>
2021-12-24 14:36:05 -0500

    moved unix aws interface scripts to appropriate repo

--- ec2/unix/ec2_ids.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-main_ec2_id=i-0cd2a955830fb3aae
-stock_ec2_id=i-0318bdd3d94e05756
--- ec2/unix/stop_stock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ec2_ids.sh
-aws ec2 stop-instances --instance-ids $stock_ec2_id
-
